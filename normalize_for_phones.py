2378a6b243ebad80875aeeae8c33fab80fa76b3b
anishnuni <anishnuni@airbears2-10-142-152-132.airbears2.1918.berkeley.edu>
2019-11-18 10:22:21 -0800

    Normalizing for different phones

---
+++ normalize_for_phones.py
@@ -0,0 +1,11 @@
+import numpy as np
+from PIL import Image
+
+standard_images = ['standard1.jpg','standard2.jpg','standard3.jpg']
+
+new_phone_test_images = ['test1.jpg','test2.jpg','test3.jpg']
+
+#can write if necessary (if the algorithm doesn't work well without this). 
+#This file should be able to figure out a transformation function
+#that can convert a new phone's images into the same image as if it was taken by
+#the phone that we are using as the standard.
